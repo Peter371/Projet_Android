b71a9e697c174dec08b15796b80344d528e02934
tboulais <thomas.tuan.boulais@gmail.com>
2017-06-10 18:26:21 +0200

    Clean lignes inutiles

--- app/src/main/java/com/cours_openclassroom/pierre/projet_android/MainActivity.java
+++ app/src/main/java/com/cours_openclassroom/pierre/projet_android/MainActivity.java
@@ -19,25 +19,17 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 
 public class MainActivity extends Activity {
-    private Button Partager;
-    private Button mUplaodBtn;
-    private ImageView mImageView;
-
-
     private static final int CAMERA_REQUEST_CODE = 1;
-    Uri uri;
-
     private StorageReference mStorage;
     private ProgressDialog mProgress;
+    private ImageView mImageView;
 
     TextView Liste_des_photos = null;
     TextView Titre_colonne_photo = null;
@@ -50,8 +42,8 @@ public class MainActivity extends Activity {
         mStorage = FirebaseStorage.getInstance().getReference();
 
         // On récupère toutes les vues dont on a besoin
-        Partager = (Button) findViewById(R.id.Button_partager);
-        Partager.setOnClickListener(new OnClickListener() {
+        Button partager = (Button) findViewById(R.id.Button_partager);
+        partager.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -66,51 +58,29 @@ public class MainActivity extends Activity {
             }
         });
 
-        mUplaodBtn = (Button) findViewById(R.id.Bupload);
+        Button mUploadBtn = (Button) findViewById(R.id.Bupload);
         mImageView = (ImageView) findViewById(R.id.imageView);
 
         mProgress = new ProgressDialog(this);
 
-        mUplaodBtn.setOnClickListener(new View.OnClickListener() {
+        mUploadBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, new ContentValues());
-
                 startActivityForResult(intent, CAMERA_REQUEST_CODE);
-
             }
         });
 
 
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        if (uri != null) {
-            outState.putString("cameraImageUri", uri.toString());
-        }
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        if (savedInstanceState.containsKey("cameraImageUri")) {
-            uri = Uri.parse(savedInstanceState.getString("cameraImageUri"));
-        }
-    }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        onRestoreInstanceState(data.getExtras());
-
         Log.e("REPONDS", "Réponds peut être?");
-
 
         if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
             if (data != null) {
@@ -118,11 +88,6 @@ public class MainActivity extends Activity {
                 mProgress.setMessage("Upload en cours ...");
                 mProgress.show();
 
-                //Log.d("KEZOKOKO", data.getData().getLastPathSegment());
-
-                //uri = data.getData();
-
-                Log.e("YOYOYOYO", uri.toString());
                 Bitmap image = (Bitmap) data.getExtras().get("data");
 
                 mImageView.setImageBitmap(image);
@@ -132,16 +97,11 @@ public class MainActivity extends Activity {
 
                 TextView mListePhoto = (TextView) findViewById(R.id.Liste_des_photos);
 
-//              CONSTAT: "data" à l'origine du problème. Type abstract mais lors de l'appel de onActivityResult pb sur getData -> null pointer exception relevée
-
-                StorageReference filepath = mStorage.child("Photos").child(uri.getLastPathSegment());
+                StorageReference filepath = mStorage.child("Photos").child(nomDeLaPhoto);
                 mListePhoto.setText(filepath.toString());
                 Log.e("WUUUUUUUUUUUUUUUUUUUUUU", filepath.toString());
 
-                // NOUVEAU & DERNIER PB (inch) ici!! l'ajout ".putFile(uri) ne marche pas
-
                 filepath.putBytes(byteArray)
-                        //.putFile(uri)
                         .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                             @Override
                             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
@@ -160,7 +120,8 @@ public class MainActivity extends Activity {
                     public void onFailure(@NonNull Exception e) {
                         // Rajouter affichage si erreur dans accès au Storage
                         mProgress.dismiss();
-                        Toast.makeText(MainActivity.this, "Ayayay\n" + uri.toString(), Toast.LENGTH_LONG).show();
+                        Toast.makeText(MainActivity.this, "Ayayay\n", Toast.LENGTH_LONG).show();
+
                         Log.d("Tristitude", e.getMessage());
                     }
                 });
